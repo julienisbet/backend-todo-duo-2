e5b76efa93568799402fdedf9a346b91050a003b
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-08-23 12:27:02 -0600

    sign up test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,46 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const mockUser = {
+  firstName: 'Cinderella',
+  lastName: 'Tremaine',
+  email: 'ilostmyshoe@cinder.com',
+  password: 'gussguss'
+
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+
+  it('Sign up form which hits POST /api/v1/users/, creates a new user and redirects to list of tasks', async () => {
+    const res = await request(app).post('/api/v1/users/')
+      .send(mockUser);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      Message: 'Hiya Cinderellie!',
+
+      user: { id: expect.any(String),
+        firstName,
+        lastName,
+        email, }
+    });
   });
+
+
+
+
+
+
+
+
+
+
   afterAll(() => {
     pool.end();
   });
