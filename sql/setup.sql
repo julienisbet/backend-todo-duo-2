20ef2b0fbd1ed5440239ea5699e58c36797f2727
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-08-24 13:55:58 -0600

    tables are set up

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE if EXISTS users cascade;
+DROP TABLE if EXISTS todos cascade;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -10,8 +11,28 @@ CREATE TABLE users (
     last_name TEXT
 );
 
+CREATE TABLE todos (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    room TEXT,
+    chore TEXT,
+    complete BOOLEAN
+
+);
+
 INSERT into users (first_name, last_name, email, password_hash) values 
 ('Snow', 'White', 'ilovepolygamy@sevenmen.com', 'apple'),
 ('Elsa', 'Oldenburg', 'canttouchthis@frozen.com', 'olaf'),
 ('Mulan', 'Fa', 'imnotawoman@warrior.com', 'mushu'),
-('Moana', 'Waialiki', 'runaway@dwayne.com', 'heihei')
+('Moana', 'Waialiki', 'runaway@dwayne.com', 'heihei');
+
+INSERT into todos (room, chore, complete) values
+('kitchen', 'dishes', 'false'),
+('kitchen', ' scrubfloors', 'false'),
+('laundry', ' wash clothes', 'false'),
+('bedrooms', 'make beds', 'false'),
+('dining', 'windows', 'false'),
+('dining', 'set the table', 'false'),
+('bathroom', 'bath pets', 'false'),
+('kitchen', 'feed pets', 'false'),
+('bathroom', 'mirrors', 'false'),
+('kitchen', 'garbage', 'false')
