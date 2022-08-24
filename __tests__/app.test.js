1bdeab1b535a9832a8c6683725351261df0456a6
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-08-24 13:40:15 -0600

    failing todo route test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -66,6 +66,12 @@ describe('backend-express-template routes', () => {
     expect(res.status).toBe(401);
   });
 
+  it ('List of tasks calls GET /api/v1/todos/ and lists all todos for the authenticated user', async () => {
+    const [agent] = await registerAndLogin();
+    const res = await agent.get('api/v1/todos');
+    expect(res.staus).toBe(200);
+  });
+
 
 
 
